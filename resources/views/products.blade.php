1095e0d768ba0e8163d91fc3c1674bf2d6f862a0
Mattymagadoo <Mattymagadoo@gmail.com>
2022-12-20 21:53:06 +0000

    Product model setup

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -2,4 +2,23 @@
  
 @section('title', 'Page Title')
 
-<h1>Product</h1>
+@section('content')
+
+<div class="inline-block bg-black w-full h-12 mb-6">
+    <ul class="text-white text-center list-none h-full block relative">
+        <li class="inline-block float-none w-auto px-5 h-full"><a href="/Assessment2/public/product" class="mx-auto text-lg">Product</a></li>
+        <li class="inline-block float-none w-auto px-5 h-full"><a href="" class="mx-auto text-lg">Pants</a></li>
+        <li class="inline-block float-none w-auto px-5 h-full"><a href="" class="mx-auto text-lg">Shoes</a></li>
+        <li class="inline-block float-none w-auto px-5 h-full"><a href="" class="mx-auto text-lg">Accessories</a></li>
+        <li class="inline-block float-none w-auto px-5 h-full"><a href="" class="mx-auto text-lg">Coats</a></li>
+    </ul>
+</div>
+<div class="container flex flex-wrap m-0 items-center justify-between mx-auto">
+    <div class=" w-1/4 h-52">
+    
+    </div>
+    <div class="w-3/4 h-52 border-solid border-slate-600 border">
+        
+    </div>
+</div>
+@stop
