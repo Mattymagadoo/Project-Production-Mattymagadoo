5f235091e3075a14f7c83b33e9b327d288828e9b
Mattymagadoo <Mattymagadoo@gmail.com>
2023-01-03 23:04:00 +0000

    test

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,15 +9,32 @@ class ProductController extends Controller
 {
     public function index(){
         $type = request('type');
+        $pg = request('pg');
+        $author = request('author');
+        $length = request('length');
         if ($type == '') {
             $products = Product::all();
+        }elseif($type == 'game'){
+            $products = Product::all()
+            ->where('type','=',$type)
+            ->where('pg', '=',$pg)
+            ->orwhere('pg','=','');
+        }elseif($type == 'book'){
+            $products = Product::all()
+            ->where('type','=',$type)
+            ->where('author', '=',$author)
+            ->orwhere('author','=','');
+        }elseif($type == 'film'){
+            $products = Product::all()
+            ->where('type','=',$type)
+            ->where('pg', '=',$pg)
+            ->orwhere('pg','=','');
         }else{
-            $products = Product::all()->where('type','=',$type); 
+            $products = Product::all()->where('type','=',$type);
         }
         
         return view('products', [
-            'products' => $products,
-            'type' => $type
+            'products' => $products
         ]);
     }
 
